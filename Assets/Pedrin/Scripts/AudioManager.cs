b33bbc96259a3ae0b1df9cfc410fde38c4501de0
Pedro Luiz <pedrin.luiz22@gmail.com>
2025-03-15 17:57:21 -0300

    Menu e AudioManager

---
+++ Assets/Pedrin/Scripts/AudioManager.cs
@@ -0,0 +1,110 @@
+using System;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance;
+
+    public Sound[] musicSounds, sfxSounds;
+    public AudioSource musicSource, sfxSource;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void PlayMusic(string name)
+    {
+        Sound s = Array.Find(musicSounds, x => x.soundName == name);
+
+        if (s == null)
+        {
+            Debug.Log("Som não encontrado");
+        }
+        else
+        {
+            musicSource.clip = s.clip;
+            musicSource.Play();
+        }
+    }
+
+    public void PlaySFX(string name)
+    {
+        Sound s = Array.Find(sfxSounds, x => x.soundName == name);
+
+        if (s == null)
+        {
+            Debug.Log("Som não encontrado");
+        }
+        else
+        {
+            sfxSource.PlayOneShot(s.clip);
+        }
+    }
+
+    public void MusicVolume(float volume)
+    {
+        musicSource.volume = volume;
+    }
+
+    public void SFXVolume(float volume)
+    {
+        sfxSource.volume = volume;
+    }
+
+    public AudioSource GetSFXAudioSource(string name)
+    {
+        Sound s = Array.Find(sfxSounds, x => x.soundName == name);
+
+        if (s == null)
+        {
+            Debug.Log("Som não encontrado: " + name);
+            return null;
+        }
+
+        // Criar um novo AudioSource para esse efeito sonoro
+        GameObject soundObject = new GameObject("SFX_" + name);
+        AudioSource newSource = soundObject.AddComponent<AudioSource>();
+
+        newSource.clip = s.clip;
+        newSource.volume = sfxSource.volume; // Herda o volume do gerenciador
+        newSource.loop = false;
+        newSource.playOnAwake = false;
+
+        // O som será destruído quando terminar de tocar
+        Destroy(soundObject, s.clip.length + 0.1f);
+
+        return newSource;
+    }
+
+    public AudioSource CreateLoopingSFX(string name)
+    {
+        Sound s = Array.Find(sfxSounds, x => x.soundName == name);
+
+        if (s == null)
+        {
+            Debug.Log("Som não encontrado: " + name);
+            return null;
+        }
+
+        GameObject soundObject = new GameObject("LoopingSFX_" + name);
+        AudioSource newSource = soundObject.AddComponent<AudioSource>();
+
+        newSource.clip = s.clip;
+        newSource.volume = sfxSource.volume;
+        newSource.loop = true; // Importante para looping
+        newSource.playOnAwake = false;
+
+        return newSource;
+    }
+
+
+}
---
+++ Assets/Pedrin/Scripts/MainMenuManager.cs
@@ -0,0 +1,52 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    [SerializeField] private string sceneName;
+    [SerializeField] private GameObject optionsPanel;
+    [SerializeField] private GameObject mainMenuPanel;
+    [SerializeField] private GameObject creditsPanel;
+
+    void Start()
+    {
+        //AudioManager.Instance.PlayMusic("Piano");
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene(sceneName);
+        //AudioManager.Instance.PlayMusic("nevasca");
+    }
+
+    public void OpenOptions()
+    {
+        optionsPanel.SetActive(true);
+        mainMenuPanel.SetActive(false);
+    }
+
+    public void CloseOptions()
+    {
+        optionsPanel.SetActive(false);
+        mainMenuPanel.SetActive(true);
+    }
+
+    public void OpenCredits()
+    {
+        creditsPanel.SetActive(true);
+        mainMenuPanel.SetActive(false);
+    }
+
+    public void CloseCredits()
+    {
+        creditsPanel.SetActive(false);
+        mainMenuPanel.SetActive(true);
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Quiting...");
+        Application.Quit();
+    }
+}
